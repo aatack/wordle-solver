4fc07d8db697eed59fb14287d567def93deeb2fd
aatack <alexanderjtatack@gmail.com>
2022-01-15 00:48:14 +0000

    Import basic word priors

--- letter_prior.py
+++ letter_prior.py
@@ -1,9 +1,20 @@
-from typing import Dict
+from collections import defaultdict
+from typing import Callable, Dict, Iterator
+
+import matplotlib.pyplot as plt
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 
 class LetterPrior:
+    @staticmethod
+    def prior(vocabulary: Callable[[], Iterator[str]], index: int) -> "LetterPrior":
+        counts = defaultdict(lambda: 0)
+        for word in vocabulary():
+            counts[word[index]] += 1
+        total = sum(counts.values())
+        return LetterPrior({letter: count / total for letter, count in counts.items()})
+
     def __init__(self, probabilities: Dict[str, float]):
         self._probabilities = probabilities
 
@@ -15,3 +26,7 @@ class LetterPrior:
         magnitude = sum(self._probabilities.values())
         for letter in self._probabilities:
             self._probabilities[letter] /= magnitude
+
+    def plot(self):
+        plt.plot(list(ALPHABET), [self[letter] for letter in ALPHABET])
+        plt.show()
--- word_prior.py
+++ word_prior.py
@@ -1,8 +1,24 @@
-from typing import List
+from typing import Callable, Iterator, List
 
-from letter_prior import LetterPrior
+import matplotlib.pyplot as plt
+
+from letter_prior import ALPHABET, LetterPrior
 
 
 class WordPrior:
+    @staticmethod
+    def prior(vocabulary: Callable[[], Iterator[str]], length: int = 5) -> "WordPrior":
+        return WordPrior([LetterPrior.prior(vocabulary, i) for i in range(length)])
+
     def __init__(self, letter_priors: List[LetterPrior]):
         self._letter_priors = letter_priors
+
+    def normalise(self):
+        for prior in self._letter_priors:
+            prior.normalise()
+
+    def plot(self):
+        _, axes = plt.subplots(len(self._letter_priors), sharex=True)
+        for prior, axis in zip(self._letter_priors, axes):
+            axis.plot(list(ALPHABET), [prior[letter] for letter in ALPHABET])
+        plt.show()
