c086f83c94418348d00c941440fc56b7d5610429
aatack <alexanderjtatack@gmail.com>
2022-05-08 00:33:57 +0100

    Improve the presentation

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ OPTIONS = ["Green", "Yellow", "Grey"]
 if "solver" not in st.session_state:
     st.session_state.solver = Solver()
 
+solver = st.session_state.solver
+
 st.write("# Wordle Solver")
 
 st.write(
@@ -32,14 +34,57 @@ if len(guess) == 5:
     if block.button("Confirm"):
         block.empty()
         with st.spinner("Loading"):
-            st.session_state.solver.guess(
-                guess, [2 - OPTIONS.index(colour) for colour in colours]
-            )
+            solver.guess(guess, [2 - OPTIONS.index(colour) for colour in colours])
 
         st.experimental_rerun()
 
 st.write("---")
 
-st.write(str(st.session_state.solver.best_answer()))
-with st.spinner("Loading best guess"):
-    st.write(str(st.session_state.solver.best_entropy()))
+left, middle, right = st.columns(3)
+
+try:
+    with left:
+        st.metric("Guesses made", solver.guesses_made)
+
+    with middle:
+        try:
+            answer, probability = solver.best_answer()
+            st.metric(
+                "Highest probability",
+                answer,
+                f"{probability * 100:.1f}%",
+                delta_color="off",
+            )
+        except ZeroDivisionError:
+            st.success("Success")
+
+    with right:
+        try:
+            with st.spinner("Loading best guess"):
+                option, entropy = solver.best_entropy()
+                st.metric(
+                    "Highest information",
+                    option,
+                    f"{(1 - entropy) * 100:.1f}",
+                    delta_color="off",
+                )
+        except ZeroDivisionError:
+            if probability >= 0.999:
+                st.success("The word has been found")
+            else:
+                st.error("The word is not in the vocabulary list")
+
+            if st.button("Reset"):
+                st.session_state.clear()
+                st.experimental_rerun()
+
+except AssertionError as e:
+    # Normally this is because the state has been invalidated by a reload
+    print("Caught assertion error:", e)
+
+    st.session_state.clear()
+    st.experimental_rerun()
+
+# Still to do:
+# - Tidy up this file
+# - Add plots of the historical data (letter/word distributions, etc.)
