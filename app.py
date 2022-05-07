f897f24c1a99386c31fedb4c4aa23ba3df0f837f
aatack <alexanderjtatack@gmail.com>
2022-05-07 02:34:17 +0100

    Make a basic UI for inputting a guess

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 
+# st.set_page_config(layout="wide")
+
 st.write("# Wordle Solver")
 
 st.write(
     "Test app for working out how to use Streamlit, and also to provide a nicer "
     "UI for the Wordle bot I wrote a few months ago."
 )
+
+
+with st.empty():
+    guess = st.text_input("Guess", max_chars=5)
+
+    if len(guess) > 0:
+        with st.container():
+            columns = st.columns([1] * len(guess))
+
+            for i, (column, character) in enumerate(zip(columns, guess)):
+                column.button(character, key=str(i))
