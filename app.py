e8cf524b5b54495ac96c2e16c4b390e9a629ad2e
aatack <alexanderjtatack@gmail.com>
2022-05-07 01:11:31 +0100

    Set up a streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("# Wordle Solver")
+
+st.write(
+    "Test app for working out how to use Streamlit, and also to provide a nicer "
+    "UI for the Wordle bot I wrote a few months ago."
+)
